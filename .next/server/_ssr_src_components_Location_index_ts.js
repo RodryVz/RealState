d3f0e848bd2d2f61840be12a9e4332d10e454f5f
rodry <rodry_valenzuela@hotmail.com>
2024-07-06 12:06:53 -0300

    comienzo Proyecto

---
+++ .next/server/_ssr_src_components_Location_index_ts.js
@@ -0,0 +1,35 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+exports.id = "_ssr_src_components_Location_index_ts";
+exports.ids = ["_ssr_src_components_Location_index_ts"];
+exports.modules = {
+
+/***/ "(ssr)/./src/components/Location/Location.tsx":
+/*!**********************************************!*\
+  !*** ./src/components/Location/Location.tsx ***!
+  \**********************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   Location: () => (/* binding */ Location)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"(ssr)/./node_modules/next/dist/server/future/route-modules/app-page/vendored/ssr/react-jsx-dev-runtime.js\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n/* __next_internal_client_entry_do_not_use__ Location auto */ \nfunction Location() {\n    return /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"p\", {\n        children: \"Location\"\n    }, void 0, false, {\n        fileName: \"C:\\\\Users\\\\Usuario\\\\Desktop\\\\rodry\\\\Archivos Programacion\\\\Proyecto Real State\\\\real-state-youtube\\\\src\\\\components\\\\Location\\\\Location.tsx\",\n        lineNumber: 7,\n        columnNumber: 9\n    }, this);\n}\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKHNzcikvLi9zcmMvY29tcG9uZW50cy9Mb2NhdGlvbi9Mb2NhdGlvbi50c3giLCJtYXBwaW5ncyI6Ijs7Ozs7OztBQUdPLFNBQVNBO0lBRVoscUJBQ0ksOERBQUNDO2tCQUFFOzs7Ozs7QUFFWCIsInNvdXJjZXMiOlsid2VicGFjazovL3JlYWwtc3RhdGUvLi9zcmMvY29tcG9uZW50cy9Mb2NhdGlvbi9Mb2NhdGlvbi50c3g/NDgzNCJdLCJzb3VyY2VzQ29udGVudCI6WyJcclxuXCJ1c2UgY2xpZW50XCJcclxuXHJcbmV4cG9ydCBmdW5jdGlvbiBMb2NhdGlvbigpIHtcclxuXHJcbiAgICByZXR1cm4gKFxyXG4gICAgICAgIDxwPkxvY2F0aW9uPC9wPlxyXG4gICAgKVxyXG59XHJcbiJdLCJuYW1lcyI6WyJMb2NhdGlvbiIsInAiXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///(ssr)/./src/components/Location/Location.tsx\n");
+
+/***/ }),
+
+/***/ "(ssr)/./src/components/Location/index.ts":
+/*!******************************************!*\
+  !*** ./src/components/Location/index.ts ***!
+  \******************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   Location: () => (/* reexport safe */ _Location__WEBPACK_IMPORTED_MODULE_0__.Location)\n/* harmony export */ });\n/* harmony import */ var _Location__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./Location */ \"(ssr)/./src/components/Location/Location.tsx\");\n\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKHNzcikvLi9zcmMvY29tcG9uZW50cy9Mb2NhdGlvbi9pbmRleC50cyIsIm1hcHBpbmdzIjoiOzs7OztBQUEyQiIsInNvdXJjZXMiOlsid2VicGFjazovL3JlYWwtc3RhdGUvLi9zcmMvY29tcG9uZW50cy9Mb2NhdGlvbi9pbmRleC50cz9lMThjIl0sInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCAqIGZyb20gXCIuL0xvY2F0aW9uXCI7XHJcbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///(ssr)/./src/components/Location/index.ts\n");
+
+/***/ })
+
+};
+;
