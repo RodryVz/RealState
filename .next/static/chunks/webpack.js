2826cc179b1837c31b3346ffb685a91a5d195bce
rodry <rodry_valenzuela@hotmail.com>
2024-07-08 13:27:34 -0300

    Banner y navbar movil

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4102d1e7159ab611"; }
+/******/ 		__webpack_require__.h = function() { return "da5472a20bbcac66"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,10 +1,14 @@
+import { FloatedSearch } from "../FloatedSearch";
+
 export function Banner() {
     return (
         <div className="container relative mx-auto">
             <div className="pt-20 md:pt-0 min-h-[80vh] bg-banner bg-no-repeat bg-cover bg-center rounded-3xl relative flex flex-col items-center md:justify-center">
                 <div className="max-w-3xl text-center text-white">
                     <h2 className=" text-3xl font-semibold">Descubre la vivienda ideal para tu familia</h2>
+                    <p className="mt-2 text-xl md:mt-8">Con mas de 30 años de experiencias, con los mejores profecionales de Real State para lo que necesiten</p>
                 </div>
+                <FloatedSearch/> 
             </div>
         </div>
     )
